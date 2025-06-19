a88fe95b7cba05c49e1ace9d0387f58d1a6a5a38
deerajirala12 <deeraj.irala04@gmail.com>
2025-06-19 15:07:13 +0530

    Updated About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,7 +13,7 @@ const About = () => {
           loop={true}
           autoPlay={true}
           style={{ width: '100%', height: '100%' }}
-        />
+      /*  />
       </div>
 
       <div className="max-w-6xl mx-auto px-6 relative z-20">
